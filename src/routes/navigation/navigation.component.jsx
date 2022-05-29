2ec0e3eca7fef784069c2f5b7aa283df9c09f89f
Damsith Karunaratna <dkarunarat@wiley.com>
2022-05-29 15:49:49 +0530

    feat(routing): :sparkles: Add react-router-dom routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+import { ReactComponent as PandaLogo } from '../../assets/Low-Poly-Panda-Head.svg';
+import './navigation.styles.scss';
+
+export const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link to='/' className='logo-container'><div><PandaLogo width={50} /></div></Link>
+        <div className='nav-links-container'>
+          <Link to='shop' className='nav-link'>SHOP</Link>
+          <Link to='sign-in' className='nav-link'>SIGN IN</Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
