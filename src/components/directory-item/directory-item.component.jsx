81439237c854fe912541ad764f84c946d1b38ebc
Damsith Karunaratna <dkarunarat@wiley.com>
2022-05-31 20:02:28 +0530

    feat(app): :sparkles: Implement routing for homepage

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,16 @@
 import { BackGroundImage, Body, DirectoryItemContainer } from './directory-item.styles.jsx';
+import { useNavigate } from 'react-router-dom';
 
-export const DirectoryItem = ({ category: { imageUrl, title } }) => {
+export const DirectoryItem = ({ category: { imageUrl, title, route } }) => {
+
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
+
   return (
     <DirectoryItemContainer >
       <BackGroundImage imageUrl={imageUrl} />
-      <Body className="directory-item-body">
-        <h2>{title}</h2>
+      <Body onClick={onNavigateHandler} className="directory-item-body">
+        <h2 >{title}</h2>
         <p>Shop now</p>
       </Body>
     </DirectoryItemContainer>
